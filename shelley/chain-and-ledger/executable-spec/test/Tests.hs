4eebd23d079782c2b53cc7cb12b6bb0312360e8f
Jared Corduan <jaredcorduan@gmail.com>
2020-02-07 15:47:57 -0500

    enabling nightly tests - #1187

--- shelley/chain-and-ledger/executable-spec/test/Tests.hs
+++ shelley/chain-and-ledger/executable-spec/test/Tests.hs
@@ -1,16 +1,26 @@
 import           Test.Tasty
 
---import           PropertyTests (propertyTests)
+import           PropertyTests (propertyTests)
 import           STSTests (stsTests)
 import           Test.Serialization (serializationTests)
 import           UnitTests (unitTests)
 
-tests :: TestTree
-tests = testGroup "Ledger with Delegation"
+ciTests :: TestTree
+ciTests = testGroup "CI_TESTS"
   [ unitTests
-  --, propertyTests
   , stsTests
   , serializationTests
+  ]
+
+nightlyTests :: TestTree
+nightlyTests = testGroup "NIGHTLY_TESTS"
+  [ propertyTests
+  ]
+
+tests :: TestTree
+tests = testGroup "Ledger with Delegation"
+  [ ciTests
+  , nightlyTests
   ]
 
 -- main entry point
